ea4ff5df07a30392f32aa04c68f2e482416f19c4
lc <1641767724@qq.com>
2026-07-31 22:23:35 +0800

    学习线程day1

--- RTSPServer/main.cpp
+++ RTSPServer/main.cpp
@@ -1,7 +1,22 @@
 #include "Thread.h"
 #include <iostream>
+#include <string>
+
+void printfHello(std::string msg)
+{
+	std::cout << msg << std::endl;
+}
 
 int main()
 {
-	std::cout << "hello" << std::endl;
+	std::thread thread1(printfHello,"hello");
+	
+	thread1.detach();
+	bool isJoin = thread1.joinable();
+	if (isJoin)
+	{
+		thread1.join();
+	}
+
+	return 0;
 }
