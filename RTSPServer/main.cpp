09d85ddba405b6757fc4b5ca80069539b23758d8
lc <1641767724@qq.com>
2026-08-01 21:54:17 +0800

    day2

--- RTSPServer/main.cpp
+++ RTSPServer/main.cpp
@@ -1,22 +1,53 @@
 #include "Thread.h"
 #include <iostream>
 #include <string>
+#include <memory>
 
-void printfHello(std::string msg)
+class A {
+public:
+	int a;
+};
+
+std::thread t2;
+void add(int &num)
 {
-	std::cout << msg << std::endl;
+	num += 1;
+}
+
+// 2:error
+void test()
+{
+	// 需要变成全局变量
+	int num = 0;
+	// 能编译但是空指针
+	t2 = std::thread(add, std::ref(num));
+}
+
+// 
+void testClass(A& a)
+{
+
 }
 
 int main()
 {
-	std::thread thread1(printfHello,"hello");
 	
-	thread1.detach();
-	bool isJoin = thread1.joinable();
-	if (isJoin)
-	{
-		thread1.join();
-	}
+	// 1:error
+	// std::thread t1(add, 1);
 
+	// 1:success
+	int num = 0;
+	std::thread t1(add, std::ref(num));
+
+	// 2:error
+	// test();
+
+	// 3:在线程里面传入对象的时候要用智能指针
+	// 这是错的 -> A a ;
+
+	std::shared_ptr<A> a = std::make_shared<A>();
+	std::thread t2(testClass, &a);
+
+	t1.join();
 	return 0;
 }
