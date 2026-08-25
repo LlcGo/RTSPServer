c3f245a9a65f9f2e36fbd1b6227c4654a9b51123
lc <1641767724@qq.com>
2026-08-25 22:12:16 +0800

    1、事件任务调度类编写

--- RTSPServer/EnventScheduler.h
+++ RTSPServer/EnventScheduler.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <stdint.h>
+#include "Timer.h"
+#include "Event.h"
 
 class Poller;
 
@@ -17,7 +23,22 @@ public:
 	virtual ~EventScheduler();
 
 public:
-	bool addTriggerEvent();
+	bool addTriggerEvent(TriggerEvent * triggerEvent);
+	Timer::TimerId addTimedEventRunAfater(TimerEvent* event, Timer::TimeInterval delay);
+	Timer::TimerId addTimedEventRunAt(TimerEvent* event, Timer::Timestamp when);
+	Timer::TimerId addTimedEventRunEvery(TimerEvent* event, Timer::TimeInterval interval);
 
+	bool removeTimeEvent(Timer::TimerId timerId);
+	bool addIOEvent(IOEvent* event);
+	bool updateIOEvent(IOEvent* event);
+	bool remove(IOEvent* event);
+
+	void loop();
+
+	Poller* poller();
+	void setTimerManagerReadCallback(EventCallback cb, void* arg);
+
+private:
+	bool mQuit;
 
 };
